507ea6c7cef36936da96074c71ff5c6de61824a4
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-04 12:50:39 +0200

    add character context to view

--- rickmorty/views.py
+++ rickmorty/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from .services import get_characters
 
 class GetInfo(TemplateView):
     template_name = 'index.html'
     def get_context_data(self, *args, **kwargs):
-        pass
+        context = {
+            'characters' : get_characters(),
+        }
+        return context
