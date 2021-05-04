bf45a1bf4dda26a84dd4e884116ed71871ce81c1
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-04 14:36:40 +0200

    add character api test

--- rickmorty/tests.py
+++ rickmorty/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from nose.tools import assert_is_not_none
+
+# Local imports...
+from .services import get_characters
+
+class CharacterTestCase(TestCase):
+    def test_request_response(self):
+        # Call the service, which will send a request to the server.
+        response = get_characters()
+
+        # If the request is sent successfully, then I expect a response to be returned.
+        assert_is_not_none(response)
