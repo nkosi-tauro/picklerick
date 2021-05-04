79776a964807fe362e23512db1c918455a5f6ba6
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-04 15:01:07 +0200

    add db cache

--- picklerick/settings.py
+++ picklerick/settings.py
@@ -81,6 +81,15 @@ DATABASES = {
     }
 }
 
+#DB Cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'c-137',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
